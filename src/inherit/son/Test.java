04d9eecae09d0f520b72366fd1aff07eadc2d338
SnowGodss <e424320741@gmail.com>
2025-02-06 02:06:34 +0800

    2025/2/6

--- src/inherit/son/Test.java
+++ src/inherit/son/Test.java
@@ -6,22 +6,40 @@ import java.util.Collections;
 public class Test {
     public static void main(String[] args) {
         /*
-         * 自動點名器1： 班級裡有N個學生, 實現隨機點名器
+         * 自動點名器2： 班級裡有N個學生, 實現隨機點名器 要求：70%的概率隨機到男生 30%的概率隨機到女生
          */
 
         // 1.創建集合
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
         // 2.批量添加元素
-        Collections.addAll(list, "小明", "小虎", "張三", "李四", "唐憎", "孫悟空");
+        Collections.addAll(list, 1, 1, 1, 1, 1, 1, 1);
+        Collections.addAll(list, 0, 0, 0);
 
         // 3.隨機打亂元素
         Collections.shuffle(list);
+        int index = list.get(0);
 
-        // 4. 取得第一個元素並取出
-        String name = list.get(0);
+        // 4.創建兩個集合來儲存男生和女生的名字
+        ArrayList<String> manlist = new ArrayList<>();
+        ArrayList<String> girllist = new ArrayList<>();
 
-        // 5. 打印元素
-        System.out.println(name);
+        Collections.addAll(manlist, "1", "2", "3", "4", "5", "6", "7");
+        Collections.addAll(girllist, "8", "9", "10");
+
+        System.out.println(list);
+
+        // 5.判斷此時是從manlist中取出名字還是girllist中取出名字
+        if (index == 1) {
+            // manlist
+            Collections.shuffle(manlist);
+            System.out.println(manlist);
+            System.out.println(manlist.get(0));
+        } else {
+            // girllist
+            Collections.shuffle(girllist);
+            System.out.println(girllist);
+            System.out.println(girllist.get(0));
+        }
     }
 }
