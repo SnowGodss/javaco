c301d4be8c4e8e728edf27a0f0d4294a3bb163f1
SnowGodss <e424320741@gmail.com>
2025-02-26 01:32:39 +0800

    2025/2/25

--- src/animal/zoo/Text.java
+++ src/animal/zoo/Text.java
@@ -1,18 +1,31 @@
 package animal.zoo;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Text {
     public static void main(String[] args) {
         /*
-         * 創建不可變的List集合 "張三", "李四", "王五", "老六"
+         * Stream流：體驗Stream流的作用 需求：按照下面的要求集合的創建和遍歷 創建一個集合, 儲存多個字符串元素
+         * 要求一：把所有已"張"開頭的元素儲存到新集合中 要求二：把"張"開頭的, 長度為3的元素再儲存到新集合中 要求三：遍歷打印結果
          */
 
-        // 一但建立完畢後, 是無法進行修改的, 在下面的代碼中, 只能進行查詢的動作
-        List<String> list = List.of("張三", "李四", "王五", "老六");
-
-        for (String s : list) {
-            System.out.println(s);
+        // 1. 創建集合
+        ArrayList<String> list1 = new ArrayList<>();
+        // 2. 添加元素
+        list1.add("張三");
+        list1.add("李四");
+        list1.add("王五");
+        list1.add("赵六");
+        list1.add("張強");
+        list1.add("張三瘋");
+        // 3. 創建新集合
+        ArrayList<String> list2 = new ArrayList<>();
+        // 4. 遍歷
+        for (String s : list1) {
+            if (s.startsWith("張")) {
+                list2.add(s);
+            }
         }
+        System.out.println(list2);
     }
 }
